c28d3fc116cc2de691e1ba6ff9f8bd45fe7bb6c3
Ayush Tiwari <101995061+aaayushh7@users.noreply.github.com>
2025-01-20 21:19:22 +0530

    helmetx

--- index.js
+++ index.js
@@ -9,6 +9,14 @@ const admin = require('firebase-admin');
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.disable('x-powered-by');
+app.use((req, res, next) => {
+  res.setHeader('X-Content-Type-Options', 'nosniff');
+  res.setHeader('X-Frame-Options', 'DENY');
+  res.setHeader('X-XSS-Protection', '1; mode=block');
+  next();
+});
+
 app.get('/', (req, res) => {
     res.json({ message: 'Welcome to the API' });
   });
